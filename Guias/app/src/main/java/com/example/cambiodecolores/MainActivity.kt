07aaa1659fe1396204a39ab8bbda4ca914fb40ac
AndrewTorrez <andrew.torrez@assuresoftbolivia.com>
2022-05-28 18:40:11 -0400

    gia completed

--- Guias/app/src/main/java/com/example/cambiodecolores/MainActivity.kt
+++ Guias/app/src/main/java/com/example/cambiodecolores/MainActivity.kt
@@ -3,30 +3,56 @@ package com.example.cambiodecolores
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-    private val redBtn : Button
-        get() = findViewById(R.id.red_btn)
-    private val whiteBtn : Button
-        get() = findViewById(R.id.white_btn)
-    private val layoutPrincipal: ConstraintLayout
-        get() = findViewById(R.id.layout_main)
+    private val serviciosBtn : Button
+        get() = findViewById(R.id.servicios_btn)
+    private val portafolioBtn : Button
+        get() = findViewById(R.id.potafolio_btn)
+    private val aboutBtn : Button
+        get() = findViewById(R.id.about_btn)
+    private val contactoBtn : Button
+        get() = findViewById(R.id.contacto_btn)
+    private val redesBtn : Button
+        get() = findViewById(R.id.redes_btn)
+
+    private val textVAriable: TextView
+        get() = findViewById(R.id.textVariable)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        redBtn.setOnClickListener{
-            toast(getString(R.string.btn_red_label))
-            layoutPrincipal.setBackgroundColor(Color.RED)
+        serviciosBtn.setOnClickListener{
+            toast(getString(R.string.btn_servicios_label))
+            textVAriable.setText("Servicios")
         }
-        whiteBtn.setOnClickListener {
-            toast(getString(R.string.btn_white_label))
-            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        portafolioBtn.setOnClickListener{
+            toast(getString(R.string.btn_portafolio_label))
+            textVAriable.setText("Portafolio")
         }
-
+        aboutBtn.setOnClickListener{
+            toast(getString(R.string.btn_about_label))
+            textVAriable.setText("Acerca De")
+        }
+        contactoBtn.setOnClickListener{
+            toast(getString(R.string.btn_contacto_label))
+            textVAriable.setText("Contacto")
+        }
+        redesBtn.setOnClickListener{
+            toast(getString(R.string.btn_redes_label))
+            textVAriable.setText("Redes Sociales")
+        }
     }
     private fun toast(text: String, duration: Int = Toast.LENGTH_LONG) =
         Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+    fun selectedButton(view: View) {
+        if ( view.id === R.id.servicios_btn) {
+            Toast.makeText( view.context, "Opción de Servicios", Toast.LENGTH_LONG).show()
+        }
+        Toast.makeText( view.context, "Evento no definido", Toast.LENGTH_LONG).show()
+    }
 }
---
+++ Guias/app/src/main/java/com/example/cambiodecolores/ServiciosActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cambiodecolores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class ServiciosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicios)
+    }
+}
+
+
